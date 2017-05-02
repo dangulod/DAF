9e2a2e8ac24ddbb03de390f44992eb57385f9432
dangulod <angulo.davi@gmail.com>
2017-05-02 21:17:52 +0200

    02 de mayo

--- R/IRR.R
+++ R/IRR.R
@@ -6,7 +6,7 @@
 #' @param Buy_Date Date of buy, with %Y-%m-%d format
 #' @param price bond price at the buy date
 #'
-#' @return
+#' @return IRR of the bond
 #' @export
 #'
 #' @examples
@@ -24,23 +24,73 @@ irr = function(nominal = nominal, Mat_Date = Mat_Date, coupon = coupon, Buy_Date
 
   }
 
-  Mat_Date = as.Date(Buy_Date)
+  Mat_Date = as.Date(Mat_Date)
   Buy_Date = as.Date(Buy_Date)
-  y = as.numeric(floor((Mat_Date - Buy_Date) / 365.25))
-  Cou_Date = seq(from = Mat_Date, by = paste("-", y + 1, " years", sep = ""), length.out = 2)[2]
-  d = as.numeric(Buy_Date - Cou_Date)
 
-  van = function(i = i, n = n, y = y, c = c, d = d, p = p) {
+  van = function(i = i, n = n, Buy_Date = Buy_Date, c = c, Mat_Date = Mat_Date, p = p) {
 
-    days = c(0, seq(from = 365 - d, by = 365, length.out = y) / 365)
-    flows = c(-p, rep(n * c, y - 1), n * c + n)
+    y = as.numeric(floor((Mat_Date - Buy_Date) / 365.25))
+    Cou_Date = seq(from = Mat_Date, by = paste("-", y, " years", sep = ""), length.out = 2)[2]
+    d = as.numeric(Cou_Date - Buy_Date) / 365
 
-    van = abs(sum(flows / (1 + i) ^ days))
+    if (y != 1) {
+
+      for (j in 1:(y - 1)) {
+        d[length(d) + 1] = j + d[1]
+      }
+    } else {
+      for (j in 1:1) {
+        d[length(d) + 1] = j + d[1]
+      }
+    }
+
+    if(d[1] != 0) {d = c(0, d)}
+
+    flows = c(-p, rep(c * n, length(d) - 1))
+    flows[length(flows)] = flows[length(flows)] + n
+
+    van = abs(sum(flows / ((1 + i) ^ d)))
 
     return(van)
   }
 
-  irr = optimize(van, c(-1, 1), n = nominal, y = y, c = coupon, d = d, p = price, tol = 1e-9)$minimum * 100
+  irr = optimize(van, c(-1, 1), n = nominal, Buy_Date = Buy_Date, c = coupon, Mat_Date = Mat_Date, p = price, tol = 1e-16)$minimum * 100
+  irr = round(irr, 6)
 
   return(irr)
 }
+
+#
+#
+# nominal = 1e4
+# coupon = 0.08
+# price = 10000
+#
+# i = irr(nominal = 1e4,
+#     Mat_Date = "2004-12-31",
+#     Buy_Date = "2003-12-31",
+#     coupon = 0.09,
+#     price = 10000) / 100
+#
+# n = nominal
+# c = coupon
+# p = price
+#
+# Mat_Date = "2004-12-31"
+# Buy_Date = "2003-09-30"
+#
+# Mat_Date = as.Date(Mat_Date)
+# Buy_Date = as.Date(Buy_Date)
+#
+# y = as.numeric(floor((Mat_Date - Buy_Date) / 365.25))
+# Cou_Date = seq(from = Mat_Date, by = paste("-", y, " years", sep = ""), length.out = 2)[2]
+# d = as.numeric(Cou_Date - Buy_Date) / 365
+#
+# for (i in 1:(y-1)) {
+#   d[length(d) + 1] = i + d[1]
+# }
+#
+# if(d[1] != 0) {d = c(0, d)}
+#
+# flows = c(-p, rep(c * n, length(d) - 1))
+# flows[length(flows)] = flows[length(flows)] + n
